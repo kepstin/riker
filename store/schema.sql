ea560093693352c9521f5f10473894290e3d6ce9
Calvin Walton <calvin.walton@kepstin.ca>
2011-10-12 07:47:40 -0400

    Reorder schema file so it actually works; add more sample data.

--- store/schema.sql
+++ store/schema.sql
@@ -1,3 +1,29 @@
+CREATE TABLE IF NOT EXISTS artist_type (
+	id		INTEGER PRIMARY KEY NOT NULL,
+	name		VARCHAR UNIQUE NOT NULL
+);
+
+INSERT OR IGNORE INTO artist_type (id, name) VALUES (1, 'Person');
+INSERT OR IGNORE INTO artist_type (id, name) VALUES (2, 'Group');
+INSERT OR IGNORE INTO artist_type (id, name) VALUES (3, 'Other');
+
+CREATE TABLE IF NOT EXISTS country (
+	id		INTEGER PRIMARY KEY NOT NULL,
+	iso_code	VARCHAR	UNIQUE NOT NULL,
+	name		VARCHAR NOT NULL
+);
+
+INSERT OR IGNORE INTO country (id, iso_code, name) VALUES (107, 'JP', 'Japan');
+
+CREATE TABLE IF NOT EXISTS gender (
+	id		INTEGER PRIMARY KEY NOT NULL,
+	name		VARCHAR UNIQUE NOT NULL
+);
+
+INSERT OR IGNORE INTO gender (id, name) values (1, 'Male');
+INSERT OR IGNORE INTO gender (id, name) values (2, 'Female');
+INSERT OR IGNORE INTO gender (id, name) values (3, 'Other');
+
 CREATE TABLE IF NOT EXISTS artist (
 	id		INTEGER PRIMARY KEY NOT NULL,
 	gid		VARCHAR UNIQUE NOT NULL,
@@ -19,6 +45,7 @@ CREATE TABLE IF NOT EXISTS artist (
 INSERT OR IGNORE INTO artist (id, gid, name, sort_name, artist_type, country) VALUES (1, 'e1719c24-118e-425b-8f94-954dcf583fb0', '40mP', '40mP', (SELECT id FROM artist_type WHERE name = 'Person'), (SELECT id FROM country WHERE iso_code = 'JP'));
 INSERT OR IGNORE INTO artist (id, gid, name, sort_name, begin_date_year, begin_date_month, begin_date_day, artist_type, gender) VALUES (2, '130d679a-9a92-4373-8348-0800b6b93a30', '初音ミク', 'Hatsune, Miku', 2007, 8, 31, (SELECT id FROM artist_type WHERE name = 'Other'), (SELECT id FROM gender WHERE name = 'Female'));
 INSERT OR IGNORE INTO artist (id, gid, name, sort_name, artist_type, gender, comment) VALUES (3, 'ef46bbef-dba2-46e3-b534-1b44cbddf249', 'GUMI', 'GUMI', (SELECT id FROM artist_type WHERE name = 'Other'), (SELECT id FROM gender WHERE name = 'Female'), 'vocaloid');
+INSERT OR IGNORE INTO artist (id, gid, name, sort_name, artist_type, country) VALUES (4, '623ea50d-00c2-4576-8303-db088e84951d', '1640mP', '1640mP', (SELECT id FROM artist_type WHERE name = 'Group'), (SELECT id FROM country WHERE iso_code = 'JP'));
 
 CREATE TABLE IF NOT EXISTS artist_credit (
 	id		INTEGER PRIMARY KEY NOT NULL,
@@ -42,22 +69,11 @@ CREATE TABLE IF NOT EXISTS artist_credit_name (
 	PRIMARY KEY (artist_credit, position)
 );
 
-CREATE TABLE IF NOT EXISTS artist_type (
-	id		INTEGER PRIMARY KEY NOT NULL,
-	name		VARCHAR UNIQUE NOT NULL
-);
-
-INSERT OR IGNORE INTO artist_type (id, name) VALUES (1, 'Person');
-INSERT OR IGNORE INTO artist_type (id, name) VALUES (2, 'Group');
-INSERT OR IGNORE INTO artist_type (id, name) VALUES (3, 'Other');
-
-CREATE TABLE IF NOT EXISTS country (
-	id		INTEGER PRIMARY KEY NOT NULL,
-	iso_code	VARCHAR	UNIQUE NOT NULL,
-	name		VARCHAR NOT NULL
-);
-
-INSERT OR IGNORE INTO country (id, iso_code, name) VALUES (107, 'JP', 'Japan');
+INSERT OR IGNORE INTO artist_credit_name (artist_credit, position, artist, name, join_phrase) VALUES (1, 1, 1, '40mP', NULL);
+INSERT OR IGNORE INTO artist_credit_name (artist_credit, position, artist, name, join_phrase) VALUES (2, 1, 1, '40mP', ' feat. ');
+INSERT OR IGNORE INTO artist_credit_name (artist_credit, position, artist, name, join_phrase) VALUES (2, 2, 2, '初音ミク', NULL);
+INSERT OR IGNORE INTO artist_credit_name (artist_credit, position, artist, name, join_phrase) VALUES (3, 1, 1, '40mP', ' feat. ');
+INSERT OR IGNORE INTO artist_credit_name (artist_credit, position, artist, name, join_phrase) VALUES (3, 2, 3, 'GUMI', NULL);
 
 CREATE TABLE IF NOT EXISTS file (
 	id		INTEGER PRIMARY KEY NOT NULL,
@@ -65,15 +81,6 @@ CREATE TABLE IF NOT EXISTS file (
 	uri		VARCHAR NOT NULL,
 	last_updated	TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-CREATE TABLE IF NOT EXISTS gender (
-	id		INTEGER PRIMARY KEY NOT NULL,
-	name		VARCHAR UNIQUE NOT NULL
-);
-
-INSERT OR IGNORE INTO gender (id, name) values (1, 'Male');
-INSERT OR IGNORE INTO gender (id, name) values (2, 'Female');
-INSERT OR IGNORE INTO gender (id, name) values (3, 'Other');
 
 CREATE TABLE IF NOT EXISTS recording (
 	id		INTEGER PRIMARY KEY NOT NULL,
